d447b9652b3112fbb170e19ead97e08737cea3d1
Edinson Ramiro Tavara Davila <etavarad@pucp.edu.pe>
2022-09-12 12:03:11 -0500

    Create Envio.java

---
+++ packetsoft/src/main/java/pe/edu/pucp/packetsoft/models/Envio.java
@@ -0,0 +1,29 @@
+package pe.edu.pucp.packetsoft.models;
+
+import java.sql.Date;
+
+import javax.persistence.Column;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+public class Envio extends BaseEntity {
+    @Column(name = "codigo_envio")
+    private String codigo_envio;
+
+    @Column(name = "fecha_hora")
+    private Date fecha_hora;
+
+    
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "id_aero_origen")
+    private Aeropuerto aero_origen;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "id_aero_destino")
+    private Aeropuerto aero_destino;
+
+
+    @Column(name = "cant_paquetes_total")
+    private int cant_paquetes_total;
+}
