2e6dd4596686af20b0b4fbf80fbf4514d5fb349c
Edinson Ramiro Tavara Davila <etavarad@pucp.edu.pe>
2022-09-12 11:33:59 -0500

    clases aeropuerto

--- packetsoft/src/main/java/pe/edu/pucp/packetsoft/models/Aeropuerto.java
+++ packetsoft/src/main/java/pe/edu/pucp/packetsoft/models/Aeropuerto.java
@@ -1,59 +1,45 @@
 package pe.edu.pucp.packetsoft.models;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "Aeropuerto")
+@SQLDelete(sql = "UPDATE aeropuerto SET activo = 0 WHERE id = ?")
+@Where(clause = "activo = 1")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+
 public class Aeropuerto extends BaseEntity {
-    private String nombre;
+    @Column(name = "cod_aeropuerto")
+    private String cod_aeropuerto;
+    @Column(name = "nombre")
+    private double nombre;
+    @Column(name = "pais")
+    private double pais;
+    @Column(name = "cod_ciudad")
+    private int cod_ciudad;
+
+    @Column(name = "longitud")
     private double longitud;
+    @Column(name = "latitud")
     private double latitud;
+    @Column(name = "capacidad_total")
     private int capacidad_total;
+    @Column(name = "capacidad_utilizado")
     private int capacidad_utilizado;
-    private Pais pais;
 
 
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public double getLongitud() {
-        return longitud;
-    }
-
-    public void setLongitud(double longitud) {
-        this.longitud = longitud;
-    }
-
-    public double getLatitud() {
-        return latitud;
-    }
-
-    public void setLatitud(double latitud) {
-        this.latitud = latitud;
-    }
-
-    public int getCapacidad_total() {
-        return capacidad_total;
-    }
-
-    public void setCapacidad_total(int capacidad_total) {
-        this.capacidad_total = capacidad_total;
-    }
-
-    public int getCapacidad_utilizado() {
-        return capacidad_utilizado;
-    }
-
-    public void setCapacidad_utilizado(int capacidad_utilizado) {
-        this.capacidad_utilizado = capacidad_utilizado;
-    }
-
-    public Pais getPais() {
-        return pais;
-    }
-
-    public void setPais(Pais pais) {
-        this.pais = pais;
-    }
+    @Column(name = "continente")
+    private Continente continente;
 }
--- packetsoft/src/main/java/pe/edu/pucp/packetsoft/models/Continente.java
+++ packetsoft/src/main/java/pe/edu/pucp/packetsoft/models/Continente.java
@@ -1,10 +1,6 @@
 package pe.edu.pucp.packetsoft.models;
-import java.util.Date;
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import org.hibernate.annotations.SQLDelete;
 import org.hibernate.annotations.Where;
