6561c745e31620c224024e778d08c39f4120ca0f
f8ac <franco.ochoa@pucp.edu.pe>
2022-09-13 01:52:29 -0500

    owo

---
+++ packetsoft/src/main/java/pe/edu/pucp/packetsoft/controllers/PaqueteController.java
@@ -0,0 +1,14 @@
+package pe.edu.pucp.packetsoft.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/paquete")
+@CrossOrigin
+public class PaqueteController {
+    
+    
+}
