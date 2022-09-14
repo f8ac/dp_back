45ca30d020bde6ea5fbd6ae4e5b0e7851dc34d24
Edinson Ramiro Tavara Davila <etavarad@pucp.edu.pe>
2022-09-14 12:11:24 -0500

    Update AeropuertoService.java

--- packetsoft/src/main/java/pe/edu/pucp/packetsoft/services/AeropuertoService.java
+++ packetsoft/src/main/java/pe/edu/pucp/packetsoft/services/AeropuertoService.java
@@ -34,12 +34,13 @@ public class AeropuertoService {
             System.out.println("hola");
             BufferedReader br = new BufferedReader(new FileReader("packetsoft/src/main/resources/aeropuertosv01.txt"));
             while((line=br.readLine()) != null) {
-                String [] data=line.split(" ");
-                Aeropuerto aero = new Aeropuerto();
-                aero.setCod_aeropuerto(data[0]);
-                aero.setNombre(data[1]);
-                aero.setPais(data[2]);
-                aero.setCod_ciudad(data[3]);
+                String linea=line.trim().replaceAll(" +", " "); // limpia multiples espacios en blanco
+                String [] data=linea.split(" "); // separa las palabras en un array
+                Aeropuerto aero = new Aeropuerto(); // crea aeropuerto
+                aero.setCod_aeropuerto(data[1]);
+                aero.setNombre(data[2]);
+                aero.setPais(data[3]);
+                aero.setCod_ciudad(data[4]);
                 aero.setZona_horaria("UTC-5");
                 aero.setNum_zona_horaria(6.0);
                 aero.setLongitud(10.0);
@@ -50,11 +51,10 @@ public class AeropuertoService {
                 Continente con = new Continente();
                 con.setId(1);
                 aero.setContinente(con);
-                System.out.println(data[0]);
+                System.out.println(data[1]);
 
                 
-                daoAeropuerto.insert(aero);
-
+                daoAeropuerto.insert(aero); // insertar cada aeropuerto
             }
             br.close();
 //        }
