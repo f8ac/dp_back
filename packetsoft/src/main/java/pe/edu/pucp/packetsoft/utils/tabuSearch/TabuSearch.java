2dc8e07775b1cf6169a1848868016343230cec62
Antony Romero <antony.romero@pucp.edu.pe>
2022-09-19 18:23:49 -0500

    tabusearch

---
+++ packetsoft/src/main/java/pe/edu/pucp/packetsoft/utils/tabuSearch/TabuSearch.java
@@ -0,0 +1,12 @@
+package pe.edu.pucp.packetsoft.utils.tabuSearch;
+
+public class TabuSearch {
+
+    int itEtapaLocal = 20;
+    int itEtapaInten = 30;
+    int itEtapaDiver = 40;
+
+    public TabuSearch(){
+        AeropuertoDao daoAeropuerto;
+    }
+}
