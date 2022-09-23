fa9e0ba3bceb5087e0f470d2e996fa4a20480e38
f8ac <65263084+f8ac@users.noreply.github.com>
2022-09-22 20:00:09 -0500

    creacion de nodos uwu

--- packetsoft/src/main/java/pe/edu/pucp/packetsoft/controllers/TestController.java
+++ packetsoft/src/main/java/pe/edu/pucp/packetsoft/controllers/TestController.java
@@ -1,23 +1,20 @@
 package pe.edu.pucp.packetsoft.controllers;
 
-import java.io.BufferedInputStream;
-import java.io.BufferedReader;
-import java.io.ByteArrayInputStream;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.multipart.MultipartFile;
 
 import pe.edu.pucp.packetsoft.models.Aeropuerto;
-import pe.edu.pucp.packetsoft.models.Continente;
+import pe.edu.pucp.packetsoft.models.Envio;
 import pe.edu.pucp.packetsoft.services.AeropuertoService;
 import pe.edu.pucp.packetsoft.services.ContinenteService;
+import pe.edu.pucp.packetsoft.services.EnvioService;
+import pe.edu.pucp.packetsoft.utils.AstarNode;
 
 @RestController
 @RequestMapping("/test")
@@ -30,80 +27,101 @@ public class TestController {
     @Autowired
     private ContinenteService continenteService;
 
-    @PostMapping(value = "/")
-    String main(@RequestParam("file") MultipartFile file){
+    @Autowired
+    private EnvioService envioService;
+
+    @PostMapping(value = "/main")
+    String main(){
+        String result = null;
+        // insert();
+        try{
+
+            // SE INSERTAN LOS AEROPUERTOS EN UNA LISTA DE NODOS DEL MISMO TAMANIO
+            List<Aeropuerto> listaAeropuertos = aeropuertoService.getAll();
+            List<AstarNode> listaNodos = Arrays.asList(new AstarNode[listaAeropuertos.size()]);
+            int i = 0;
+            for (Aeropuerto aeropuerto : listaAeropuertos) {
+                AstarNode nodo = new AstarNode(0);
+                nodo.setAeropuerto(aeropuerto);
+                listaNodos.set(i,nodo);
+                i++;
+            }
+
+            // SE INSERTAN LOS COSTOS DE LOS VERTICES EN ORDEN DE LLEGADA
+            List<Envio> listaEnvios = envioService.getAll();
+            for (Envio envio : listaEnvios) {
+                aeropuertoService.get(envio.getAero_destino().getId());
+            }
+            result = "insertado xd";
+        }catch(Exception ex){
+            System.err.println(ex.getMessage());
+        }
+        
+
+
+
+        return result;
+    }
+
+    @PostMapping(value = "/insert")
+    String insert(){
+        String result = null;
+        try{
+
+            continenteService.insertTodos();
+            aeropuertoService.insertfile();
+            envioService.insertfile();
+        }catch(Exception ex){
+            System.err.println(ex.getMessage());
+            result = "error lol";
+        }
+        return result;
+    }
+
+
+
+    /*   
+    @PostMapping(value = "/obsolete")
+    String obsoleteMains(@RequestParam("file") MultipartFile file){
         
         try{
-            // String fileBuffer = new String(file.getBytes());
-            // FileInputStream inputStream = new FileInputStream(file);
             InputStream targeStream = new ByteArrayInputStream(file.getBytes());
             BufferedInputStream buffer = new BufferedInputStream(targeStream);
-
             BufferedReader reader = new BufferedReader(new InputStreamReader(buffer));
             String line;
             reader.readLine();
             reader.readLine();
             reader.readLine();
-
             while(reader.ready()){
                 line = reader.readLine();
-
                 String nombreContinente = line.trim();
-
                 Continente continente = new Continente();
                 continente.setNombre(nombreContinente);
                 Continente result = continenteService.insert(continente);
-
                 while(true){
-
                     line = reader.readLine();
                     if(line == null) break;
                     if(line == null || line.isEmpty() ){
                         break;
                     }
-
-                    //String numAerpuertoStr;
-                    //Integer numAeropuerto;
                     String codAeropuerto;
-                    //String nomCiudad;
                     String nomPais;
                     String codCiudad;
-
-                    //numAerpuertoStr = line.substring(0, 5).trim();
                     codAeropuerto = line.substring(4, 11).trim();
-                    //nomCiudad = line.substring(12, 31).trim();
                     nomPais = line.substring(32, 47).trim();
                     codCiudad = line.substring(48, 52).trim();
-
                     Aeropuerto aeropuerto = new Aeropuerto();
                     aeropuerto.setCod_aeropuerto(codAeropuerto);
                     aeropuerto.setCod_ciudad(codCiudad);
                     aeropuerto.setPais(nomPais);
                     aeropuerto.setContinente(result);
-
                     aeropuertoService.insert(aeropuerto);
-
-
                 }
             }
-
-
-
-            // for (Continente continente : listaContinente) {
-            //     System.out.println(continente.getNombre());
-            // }
-            // System.out.println(fileBuffer);
         }catch(Exception ex){
             System.err.println(ex.getMessage());
         }
         return "main";
-    }
-
-
-    // private void lineToStr(String line, String result, Integer start, Integer end){
-    //     result = new String();
-    //     for (int i = start, j = 0; i < end + 1; i++,j++) {
-    //         result.charAt(j) = line.charAt(i);
-    //     }
-    // }
+    } 
+    */
 }
