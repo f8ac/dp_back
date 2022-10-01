1e43769c3356a494f8aeb8541c430918d2dd2629
f8ac <65263084+f8ac@users.noreply.github.com>
2022-10-01 17:26:02 -0500

    owo

--- packetsoft/src/main/java/pe/edu/pucp/packetsoft/services/VueloService.java
+++ packetsoft/src/main/java/pe/edu/pucp/packetsoft/services/VueloService.java
@@ -42,54 +42,107 @@ public class VueloService {
     
 
     public void insertfile() throws IOException, InterruptedException {
-        String line="";
-        BufferedReader br = new BufferedReader(new FileReader("src/main/resources/vuelosv01.txt"));
-        while((line=br.readLine()) != null) {
-            String [] data=line.split("-"); // separa las palabras en un array
+        int mes = Calendar.AUGUST, dia = 2, anio = 2022;
+        for (int i = 1; i < 10; i++) {
+            String line="";
+            BufferedReader br = new BufferedReader(new FileReader("src/main/resources/vuelosv01.txt"));
+            while((line=br.readLine()) != null) {
+                String [] data=line.split("-"); // separa las palabras en un array
 
-            // aeropuerto de salida y llegada
-            Aeropuerto aeroSalida = new Aeropuerto();
-            Aeropuerto aeroLlegada = new Aeropuerto();
-            aeroSalida.setCod_aeropuerto(data[0]);
-            aeroLlegada.setCod_aeropuerto(data[1]);
-            Aeropuerto aeroSalidaDefinido = aeropuertoService.getByCodigo(aeroSalida);
-            Aeropuerto aeroLlegadaDefinido = aeropuertoService.getByCodigo(aeroLlegada);
+                // aeropuerto de salida y llegada
+                Aeropuerto aeroSalida = new Aeropuerto();
+                Aeropuerto aeroLlegada = new Aeropuerto();
+                aeroSalida.setCod_aeropuerto(data[0]);
+                aeroLlegada.setCod_aeropuerto(data[1]);
+                Aeropuerto aeroSalidaDefinido = aeropuertoService.getByCodigo(aeroSalida);
+                Aeropuerto aeroLlegadaDefinido = aeropuertoService.getByCodigo(aeroLlegada);
+
+                int horaSalida  = Integer.parseInt(data[2].split(":")[0]);
+                int horaLlegada = Integer.parseInt(data[3].split(":")[0]);
+                int zonaHorariaSalida  =  aeroSalidaDefinido.getNum_zona_horaria().intValue();
+                int zonaHorariaLlegada = aeroLlegadaDefinido.getNum_zona_horaria().intValue();
+                int horaSalidaUTC,horaLlegadaUTC;
 
 
-            Calendar calendarioSalida = Calendar.getInstance(); // tiempos de salida y llegada del vuelo
-            calendarioSalida.set(Calendar.HOUR_OF_DAY, Integer.parseInt(data[2].split(":")[0])-aeroSalidaDefinido.getNum_zona_horaria().intValue());
-            calendarioSalida.set(Calendar.MINUTE, Integer.parseInt(data[2].split(":")[1]));
-            Calendar calendarioLlegada = Calendar.getInstance();
-            calendarioLlegada.set(Calendar.HOUR_OF_DAY, Integer.parseInt(data[3].split(":")[0])-aeroLlegadaDefinido.getNum_zona_horaria().intValue());
-            calendarioLlegada.set(Calendar.MINUTE, Integer.parseInt(data[3].split(":")[1]));
+                int diaModifSalida = dia + i;
+                
+
+                if(zonaHorariaSalida > horaSalida){
+                    horaSalidaUTC = 24 - (horaSalida - zonaHorariaSalida);
+                    diaModifSalida --;
+                }else{
+                    horaSalidaUTC = (horaSalida - zonaHorariaSalida);
+                    if(horaSalidaUTC > 24){
+                        horaSalidaUTC -= 24;
+                        diaModifSalida++;
+                    }
+                }
+
+                int diaModifLlegada = diaModifSalida;
+
+                if(zonaHorariaLlegada > horaLlegada ){
+                    horaLlegadaUTC = 24 - (horaLlegada - zonaHorariaLlegada);
+                }else{
+                    horaLlegadaUTC = horaLlegada - zonaHorariaLlegada;
+                    if(horaLlegadaUTC > 24){
+                        horaLlegadaUTC -= 24;
+                    }
+                }
+
+                int minutos_salida  = horaSalidaUTC  * 60 + Integer.parseInt(data[2].split(":")[1]);
+                int minutos_llegada = horaLlegadaUTC * 60 + Integer.parseInt(data[3].split(":")[1]);
+
+                if(minutos_salida > minutos_llegada){
+                    diaModifLlegada++;
+                }
 
 
-            Vuelo vuelo = new Vuelo(); // creacion del vuelo
+                Calendar calendarioSalida = Calendar.getInstance(); // tiempos de salida y llegada del vuelo
+                calendarioSalida.set(Calendar.HOUR_OF_DAY, horaSalidaUTC);
+                calendarioSalida.set(Calendar.MINUTE, Integer.parseInt(data[2].split(":")[1]));
+                Calendar calendarioLlegada = Calendar.getInstance();
+                calendarioLlegada.set(Calendar.HOUR_OF_DAY, horaLlegadaUTC);
+                calendarioLlegada.set(Calendar.MINUTE, Integer.parseInt(data[3].split(":")[1]));
+                
+                calendarioSalida .set(Calendar.YEAR, anio);
+                calendarioLlegada.set(Calendar.YEAR, anio);
+                calendarioSalida .set(Calendar.MONTH, mes);
+                calendarioLlegada.set(Calendar.MONTH, mes);
 
+                calendarioSalida .set(Calendar.DAY_OF_MONTH, diaModifSalida);
+                calendarioLlegada.set(Calendar.DAY_OF_MONTH,diaModifLlegada);
+                
+
+
+                int tiempo_vuelo_minutos = minutos_llegada - minutos_salida;
+
+                Vuelo vuelo = new Vuelo(); // creacion del vuelo
+
+                if(tiempo_vuelo_minutos < 0){
+                    tiempo_vuelo_minutos = 1440+tiempo_vuelo_minutos;
+                }
+                if(tiempo_vuelo_minutos > 1440){
+                    tiempo_vuelo_minutos -= 1440;
+                }
+                vuelo.setTiempo_vuelo_minutos(tiempo_vuelo_minutos);
+                vuelo.setHora_salida(calendarioSalida.getTime());
+                vuelo.setHora_llegada(calendarioLlegada.getTime());
+                vuelo.setCapacidad_utilizada(0);
+                vuelo.setCapacidad_total(100);
+                vuelo.setAeropuerto_salida(aeroSalidaDefinido);
+                vuelo.setAeropuerto_llegada(aeroLlegadaDefinido);
+
+                daoEmpresa.insert(vuelo); // inserta el vuelo
+
+            }
+            br.close();
             
-
-            int minutos_salida =  (Integer.parseInt(data[2].split(":")[0])- aeroSalidaDefinido.getNum_zona_horaria().intValue()) * 60 + Integer.parseInt(data[2].split(":")[1]);
-            int minutos_llegada = (Integer.parseInt(data[3].split(":")[0])-aeroLlegadaDefinido.getNum_zona_horaria().intValue()) * 60 + Integer.parseInt(data[3].split(":")[1]);
-
-            int tiempo_vuelo_minutos = minutos_llegada - minutos_salida;
-
-            if(tiempo_vuelo_minutos < 0){
-                tiempo_vuelo_minutos += 24 * 60;
-            }
-            if(tiempo_vuelo_minutos > 1440){
-                tiempo_vuelo_minutos -= 1440;
-            }
-            vuelo.setTiempo_vuelo_minutos(tiempo_vuelo_minutos);
-            vuelo.setHora_salida(calendarioSalida.getTime());
-            vuelo.setHora_llegada(calendarioLlegada.getTime());
-            vuelo.setCapacidad_utilizada(0);
-            vuelo.setCapacidad_total(100);
-            vuelo.setAeropuerto_salida(aeroSalidaDefinido);
-            vuelo.setAeropuerto_llegada(aeroLlegadaDefinido);
-
-            daoEmpresa.insert(vuelo); // inserta el vuelo
         }
-        br.close();
+        
     }
 
+
+    public void insertTenDays() throws IOException, InterruptedException{
+
+    }
 }
