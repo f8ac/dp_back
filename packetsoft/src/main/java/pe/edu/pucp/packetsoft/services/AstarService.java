7d51cf07dc5fd84dab5a80af7b43487dec1fff3c
f8ac <franco.ochoa@pucp.edu.pe>
2022-10-24 18:22:40 -0500

    owo

--- packetsoft/src/main/java/pe/edu/pucp/packetsoft/services/AstarService.java
+++ packetsoft/src/main/java/pe/edu/pucp/packetsoft/services/AstarService.java
@@ -3,6 +3,7 @@ package pe.edu.pucp.packetsoft.services;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import pe.edu.pucp.packetsoft.models.Aeropuerto;
