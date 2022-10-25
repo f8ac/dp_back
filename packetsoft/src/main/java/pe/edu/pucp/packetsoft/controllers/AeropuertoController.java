0323a5c4aa8da42a02ac4a8181586cf677ba2718
Edinson Ramiro Tavara Davila <etavarad@pucp.edu.pe>
2022-10-24 23:47:14 -0500

    Update AeropuertoController.java

--- packetsoft/src/main/java/pe/edu/pucp/packetsoft/controllers/AeropuertoController.java
+++ packetsoft/src/main/java/pe/edu/pucp/packetsoft/controllers/AeropuertoController.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/aeropuerto")
-@CrossOrigin
+@CrossOrigin("*")
 public class AeropuertoController {
     @Autowired
     private AeropuertoService aeropuertoService;
