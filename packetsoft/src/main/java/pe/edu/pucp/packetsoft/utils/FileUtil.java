b2ca1436587e3b8fb00b748e0981e267c8a09530
Edinson Ramiro Tavara Davila <etavarad@pucp.edu.pe>
2022-10-27 21:43:48 -0500

    Update FileUtil.java

--- packetsoft/src/main/java/pe/edu/pucp/packetsoft/utils/FileUtil.java
+++ packetsoft/src/main/java/pe/edu/pucp/packetsoft/utils/FileUtil.java
@@ -4,12 +4,12 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class FileUtil {
-    private FileUtil() {
-        // restrict instantiation
-      }
-    
-      //public static final String folderPath =  "/dp_backincoming-files//";
-      public static final String folderPath =  "dp_back/packetsoft/src/main/resources/incoming-files/";
-      public static final Path filePath = Paths.get(folderPath);
-    
+  private FileUtil() {
+    // restrict instantiation
+  }
+
+  // public static final String folderPath = "/dp_backincoming-files//";
+  public static final String folderPath = "src/main/resources/incoming-files/";
+  public static final Path filePath = Paths.get(folderPath);
+
 }
