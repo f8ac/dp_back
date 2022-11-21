87f0e07b0f2c37e101d654a2c4aefebfa29f023d
f8ac <franco.ochoa@pucp.edu.pe>
2022-11-21 18:31:35 -0500

    owo

--- packetsoft/apush.bat
+++ packetsoft/apush.bat
@@ -1,4 +1,4 @@
-scp -i llave.pem -r src/main/resources/* ubuntu@"44.206.101.117":/home/ubuntu
+scp -i llave.pem -r src/main/resources/* ubuntu@"44.206.101.117":/home/ubuntu/src/main/resources
 cd target
 scp -i llave.pem packetsoft-0.0.1-SNAPSHOT.jar ubuntu@"44.206.101.117":/home/ubuntu
 cd ..
