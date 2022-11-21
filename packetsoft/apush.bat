07e2b66e9dc4942723defc6ebc289b51ad5ecb77
f8ac <franco.ochoa@pucp.edu.pe>
2022-11-21 18:07:20 -0500

    owo

--- packetsoft/apush.bat
+++ packetsoft/apush.bat
@@ -1,3 +1,5 @@
+scp -i llave.pem -r src/main/resources/* ubuntu@"44.206.101.117":/home/ubuntu
 cd target
-scp -i llave.pem -r src ubuntu@"44.206.101.117":/home/ubuntu
 scp -i llave.pem packetsoft-0.0.1-SNAPSHOT.jar ubuntu@"44.206.101.117":/home/ubuntu
+cd ..
+
--- packetsoft/src/main/java/pe/edu/pucp/packetsoft/controllers/EnvioController.java
+++ packetsoft/src/main/java/pe/edu/pucp/packetsoft/controllers/EnvioController.java
@@ -3,11 +3,14 @@ package pe.edu.pucp.packetsoft.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import pe.edu.pucp.packetsoft.models.Aeropuerto;
 import pe.edu.pucp.packetsoft.models.Envio;
+import pe.edu.pucp.packetsoft.services.AeropuertoService;
 import pe.edu.pucp.packetsoft.services.EnvioService;
 
 import java.io.IOException;
 import java.text.ParseException;
+import java.util.Hashtable;
 import java.util.List;
 
 @RestController
@@ -16,6 +19,8 @@ import java.util.List;
 public class EnvioController {
     @Autowired
     private EnvioService envioService;
+    @Autowired
+    private AeropuertoService aeropuertoService;
     
     @GetMapping(value = "/list")
     List<Envio> getAll(){
@@ -42,6 +47,34 @@ public class EnvioController {
     @PostMapping(value = "/list/orden/fecha")
     List<Envio> listOrdenFecha(){
         return envioService.listOrdenFecha();
+    }
+
+    @PostMapping(value = "/list/table")
+    List<Envio> main(@RequestBody Prm param){
+        List<Envio> result = null;
+        try{
+            List<Aeropuerto> listaAeropuertos = aeropuertoService.getAll();
+            Hashtable<String, Aeropuerto> aeroHash = airportToHash(listaAeropuertos);
+            List<Envio> listaEnvios = envioService.readFilesToLocal(param,aeroHash);
+            result = listaEnvios;
+        }catch(Exception ex){
+            System.err.println(ex.getMessage());
+        }
+        return result;
+    }
+
+    Hashtable<String, Aeropuerto> airportToHash(List<Aeropuerto> lista){
+        Hashtable<String, Aeropuerto> result = null;
+        try{
+            Hashtable<String, Aeropuerto> aeroHash = new Hashtable<>();
+            for (Aeropuerto aeropuerto : lista) {
+                aeroHash.put(aeropuerto.getCod_aeropuerto(), aeropuerto);
+            }
+            result = aeroHash;
+        }catch(Exception ex){
+            System.err.println(ex.getMessage());
+        }
+        return result;
     }
 
 }
